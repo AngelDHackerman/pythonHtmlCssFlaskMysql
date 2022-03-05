56102a13dceef18cd20d78407797e644d61e0cfe
angelDariaux <angeldariaux@gmail.com>
2022-03-04 21:50:12 -0600

    concatenacion

--- intro-python/index.py
+++ intro-python/index.py
@@ -1,3 +1,6 @@
+from curses import init_color
+
+
 if 5 > 3:                    # * este es el operador mayor que > 
   print('5 es mayor a 3')
   
@@ -15,8 +18,19 @@ print(email)
 
 miVar = 'chanchito'
 
+
   # ? Multiples variables 
 
 a, b, c = 'lala', 'lele', 'lolo'
-
 print(a, b, c);
+
+valor1 = valor2 = valor3 = 'chanchito feliz'
+print(valor1, valor2, valor3)
+
+
+  # ? Concatenacion
+
+inicio = 'Hola '
+final = 'mundo'
+
+print(inicio + final);
