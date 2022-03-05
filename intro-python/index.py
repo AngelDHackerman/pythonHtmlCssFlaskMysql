5dca415c1675210784cf4452142605004f629077
angelDariaux <angeldariaux@gmail.com>
2022-03-04 21:39:58 -0600

    variables, operadores en python

---
+++ intro-python/index.py
@@ -0,0 +1,18 @@
+if 5 > 3:                    # * este es el operador mayor que > 
+  print('5 es mayor a 3')
+  
+if 3 > 6: 
+  print('Esto no se va a imprimir')
+
+  # ? Variables
+
+x = 6
+y = 'chanchito feliz'
+print(x, y)
+
+email = 'angel@angel.com'
+print(email)
+
+miVar = 'chanchito'
+
+
