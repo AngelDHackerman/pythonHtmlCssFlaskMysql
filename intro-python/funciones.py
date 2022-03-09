eec79d352bd11cf6a150b4eae93c3ab49bf65bb6
angelDariaux <angeldariaux@gmail.com>
2022-03-08 22:09:17 -0600

    creando funciones

---
+++ intro-python/funciones.py
@@ -0,0 +1,15 @@
+            # ? Funciones
+
+def miFuncion():
+  print('Mi primera funcion en python')
+
+miFuncion()
+
+
+def imprimeDato(dato):
+  print('El argumento es: ',dato)
+
+imprimeDato('hola k ase')
+imprimeDato(1234)
+
+
