c22218819985223e3a570c7c6485d5e9f9112153
angelDariaux <angeldariaux@gmail.com>
2022-03-09 21:03:57 -0600

    usando funciones y kwargs para pasar parametros sin importar el orden

--- intro-python/funciones.py
+++ intro-python/funciones.py
@@ -6,10 +6,40 @@ def miFuncion():
 miFuncion()
 
 
-def imprimeDato(dato):
-  print('El argumento es: ',dato)
+def imprimeNombre (nombre, apellido): 
+  print('El nombre completo es: ', nombre, apellido)
 
-imprimeDato('hola k ase')
-imprimeDato(1234)
+imprimeNombre('Angel ', 'Hernandez')
+
+          # ? usando parametros variables (como para imprimer una tupla)
+
+def imprimeVarios (*nombre): 
+  print('Los datos recibidos son: ', nombre)
+
+imprimeVarios('Angel', 'Lola', 'Esteban', 'Marco') # Los datos recibidos son:  ('Angel', 'Lola', 'Esteban', 'Marco')
+
+
+          # ? Pasando argumentos usando los parametros sin importar el orden
+
+def nombreCompleto (apellido, nombre):
+  print('El nombre completo es: ', nombre, apellido)
+
+# nombreCompleto(nombre='Angel ', apellido='Hernandez')
+
+def nombreCompleto2 (**kwargs):
+  print('El nombre completo es: ', kwargs['nombre'], kwargs['apellido'])
+
+nombreCompleto2(nombre='Angel ', apellido='Hernandez 2')
+
+
+
+
+
+
+
+
+
+
+
 
 
