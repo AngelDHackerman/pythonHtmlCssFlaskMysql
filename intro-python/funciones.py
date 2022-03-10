36ac21763836439226eac064cb89b50d5b72658e
angelDariaux <angeldariaux@gmail.com>
2022-03-09 21:17:19 -0600

    pasando listas como argumentos y usando el ciclo for in

--- intro-python/funciones.py
+++ intro-python/funciones.py
@@ -3,20 +3,20 @@
 def miFuncion():
   print('Mi primera funcion en python')
 
-miFuncion()
+# miFuncion()
 
 
 def imprimeNombre (nombre, apellido): 
   print('El nombre completo es: ', nombre, apellido)
 
-imprimeNombre('Angel ', 'Hernandez')
+# imprimeNombre('Angel ', 'Hernandez')
 
           # ? usando parametros variables (como para imprimer una tupla)
 
 def imprimeVarios (*nombre): 
   print('Los datos recibidos son: ', nombre)
 
-imprimeVarios('Angel', 'Lola', 'Esteban', 'Marco') # Los datos recibidos son:  ('Angel', 'Lola', 'Esteban', 'Marco')
+# imprimeVarios('Angel', 'Lola', 'Esteban', 'Marco') # Los datos recibidos son:  ('Angel', 'Lola', 'Esteban', 'Marco')
 
 
           # ? Pasando argumentos usando los parametros sin importar el orden
@@ -29,13 +29,34 @@ def nombreCompleto (apellido, nombre):
 def nombreCompleto2 (**kwargs):
   print('El nombre completo es: ', kwargs['nombre'], kwargs['apellido'])
 
-nombreCompleto2(nombre='Angel ', apellido='Hernandez 2')
+# nombreCompleto2(nombre='Angel ', apellido='Hernandez 2')
 
 
+          # ? pasando parametros por defecto 
+
+def miFuncion2 (argumento = 'Chanchito feliz'):
+  print(argumento)
+
+# miFuncion2('Batman')
+# miFuncion2();          
+
+          # ? pasando listas como argumentos
+
+def miFuncionLista (lista): 
+  for elemt in lista:
+    print (elemt)
+
+# miFuncionLista(['Hola', 'Que', 'hace',])
 
 
+def concatenaNombres (lista): 
+  i = ''
+  for elemt in lista:
+    i = i + elemt + ' '
+  return i
 
-
+nombres = concatenaNombres(['Hola', 'Que', 'hace',])
+print(nombres)
 
 
 
