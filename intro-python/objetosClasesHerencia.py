3712de4cef86bd7caeaa4c450ccc61144cb3b922
angelDariaux <angeldariaux@gmail.com>
2022-03-09 22:18:30 -0600

    Herencia en python

--- intro-python/objetosClasesHerencia.py
+++ intro-python/objetosClasesHerencia.py
@@ -2,6 +2,8 @@
 Las clases, son como los planos de una casa, nos dicen cuantas habitaciones van a tener, y como van a ser construidas.
 Las instancias, son las casas construidas usando el plano que creamos con la clase.
 
+En Herencia, el metodo super no da acceso a los metodos de la clase donde esta basada. como un (super usuario)
+
 """
 
           # ? Creando clases en python
@@ -24,13 +26,15 @@ class Usuario2:
     self.nombre = nombre
     self.apellido = apellido
 
-  def saludo(self):                                     # * asi se crean los metodos
+  def saludo(self):                                     # * asi se crean los METODOS
     print('Hola, mi nombre es:', self.nombre, self.apellido)   
   
+class Admin(Usuario2):                                  # ? Asi se crea la herencia
+  def superSaludo(self):
+    print('Hola!, me llamo,', self.nombre, ' y soy administrador')
 
 
-
-alumno = Usuario2('Felipe', 'Feliz')
+alumno = Usuario2('Felipe', 'Feliz')                    # * asi se crean las INSTANCIAS 
 
 # print(alumno.nombre, alumno.apellido)
 # alumno.saludo()
@@ -40,6 +44,14 @@ alumno.nombre = 'Juanito'
 alumno.saludo()
 
 # del usuario.nombre              # todo:  con la palabra "del" eliminamos el metodo o propiedad que deseamos
+
+
+admin = Admin('Super', 'Feliz')
+admin.saludo()
+admin.superSaludo()
+
+
+
 
 
 
