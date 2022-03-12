1cc7b1245e33ac066042d178194e8c0229db1c3b
angelDariaux <angeldariaux@gmail.com>
2022-03-11 20:51:13 -0600

    aprendiendo a eliminar y editar archivos desde python

--- intro-python/archivos.py
+++ intro-python/archivos.py
@@ -1,25 +1,41 @@
 
-          # * Leyendo archivos en python
+#           # * Leyendo archivos en python
 
-c = open('./chanchito.txt')
+# c = open('./chanchito.txt', 'a')
 
-# print(c.read())
+# # print(c.readline())
 
 
-          # * leer lineas 1 a 1
+#           # * leer lineas 1 a 1
 
-print(c.readline())
-print(c.readline())
-print(c.readline())
+# # print(c.read())
 
-for x in c:             # ? Asi podemos leer todas las lineas usando el ciclo for
-  print(x)
+# for x in c:             # ? Asi podemos leer todas las lineas usando el ciclo for
+#   print(x)
 
 
 
-c.close()                     # ? Siempre es bueno darle close a los archivos leidos
+# c.close()                     # ? Siempre es bueno darle close a los archivos leidos
+
+# # c.write('Agregaremos una nueva linea al nuestro archivo')         # ? Asi agregamos texto al documento
+
+# x = open ('./chanchito.txt', 'r')
+
+# print(x.read())
 
 
+                    # ? Eliminando archivos y carpetas 
+
+import os
+
+if os.path.exists('chanchito.txt'):
+  os.remove('./chanchito.txt')        # * Asi se eliminan los archivos usando python
+else: 
+  print('El archivo no existe')
 
 
+if os.path.exists('miCarpeta'):
+  os.rmdir('./miCarpeta')
+else:
+  print('La carpeta no existe')
 
